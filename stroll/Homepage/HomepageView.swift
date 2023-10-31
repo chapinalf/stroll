11cee4960edf24d06c6799392d10708ee2fda7f9
Chapin <116377406+chapinalf@users.noreply.github.com>
2023-10-30 22:52:11 -0400

    Setting up app homepage

---
+++ stroll/Homepage/HomepageView.swift
@@ -0,0 +1,20 @@
+//
+//  Homepage.swift
+//  stroll
+//
+//  Created by Chapin Alf on 10/30/23.
+//
+
+import UIKit
+
+class Homepage: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
