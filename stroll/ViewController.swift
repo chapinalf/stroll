9ee5d0963e59e83b9fc32756c5b2308d2b339dec
Chapin <116377406+chapinalf@users.noreply.github.com>
2023-10-30 22:47:19 -0400

    Initial Commit

---
+++ stroll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stroll
+//
+//  Created by Chapin Alf on 10/30/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
